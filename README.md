b0e611f13cf60981d48c20b2246f25b3c43085e2
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2023-05-05 13:12:26 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Magic Auth Microsoft Social Login
+
+Please [click here](https://magic.link/docs/auth/login-methods/social-logins/integration/social-providers/microsoft) for full implementation instructions
---
+++ src/pages/Login.js
@@ -0,0 +1,27 @@
+import { magic } from "../lib/magic";
+import { FaMicrosoft } from "react-icons/fa";
+
+const Login = () => {
+  const handleSocialLogin = async () => {
+    try {
+      await magic.oauth.loginWithRedirect({
+        provider: "microsoft",
+        redirectURI: new URL("/dashboard", window.location.origin).href,
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div className="container">
+      <h1>Magic Auth + Microsoft</h1>
+      <button onClick={handleSocialLogin}>
+        <FaMicrosoft size={"2.5rem"} />
+        Log in with Microsoft
+      </button>
+    </div>
+  );
+};
+
+export default Login;
